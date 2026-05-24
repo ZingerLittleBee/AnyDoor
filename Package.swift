828d08697b826bbd7fd1890f31ac37fc11af1677
ZingerLittleBee <6970999@gmail.com>
2026-05-24 19:03:17 +0800

    feat(brightness): add DDC.swift dependency for external display DDC/CI control

--- Package.swift
+++ Package.swift
@@ -13,6 +13,10 @@ let package = Package(
             url: "https://github.com/sparkle-project/Sparkle",
             exact: "2.9.2"
         ),
+        .package(
+            url: "https://github.com/reitermarkus/DDC.swift",
+            branch: "master"
+        ),
     ],
     targets: [
         .executableTarget(
@@ -20,6 +24,7 @@ let package = Package(
             dependencies: [
                 .product(name: "AskForPermission", package: "AskForPermission"),
                 .product(name: "Sparkle", package: "Sparkle"),
+                .product(name: "DDC", package: "DDC.swift"),
             ],
             swiftSettings: [
                 .swiftLanguageMode(.v6),
